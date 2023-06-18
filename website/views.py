335567510880c9e4129c80aab2c77d058eb00938
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2023-06-18 12:04:32 -0700

    add HTML base files

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint #has a bunch of routes
+from flask import Blueprint, render_template #has a bunch of routes
 
 views = Blueprint('views', __name__)
 
 #name of the Blueprint
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
